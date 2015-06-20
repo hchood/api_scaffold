d5f5def6eca46ddd937894f1e8185cc2b60079b8
Helen Hood <helen.c.hood@gmail.com>
2015-06-20 18:55:52 -0400

    Get rid of web stuff in router
    Don’t need to browser pipeline if just rendering JSON.

--- web/router.ex
+++ web/router.ex
@@ -1,25 +1,14 @@
 defmodule ApiScaffold.Router do
   use ApiScaffold.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", ApiScaffold do
-    pipe_through :browser # Use the default browser stack
+  # Other scopes may use custom stacks.
+  scope "/api", ApiScaffold do
+    pipe_through :api
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", ApiScaffold do
-  #   pipe_through :api
-  # end
 end
