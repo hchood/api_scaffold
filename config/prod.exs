feb8a19752cf02bfd129ba24ee79dcef4865b232
Helen Hood <helen.c.hood@gmail.com>
2015-06-20 23:00:29 -0400

    Don't need static stuff for API

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :api_scaffold, ApiScaffold.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "example.com"]
 
 # ## SSL Support
 #
